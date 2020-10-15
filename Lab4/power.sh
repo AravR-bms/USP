acdc487560524727149a4253b712d70eb736c8cb
Aravindh RS <aravindh.ravishankar@gmail.com>
2020-10-15 15:17:52 +0530

    Week 4

--- Lab4/power.sh
+++ Lab4/power.sh
@@ -3,10 +3,9 @@ read num power
 
 i=0
 ans=1
-while [ $i -ne $power ]
-do
-    ans=$((ans*num))
-    i=$((i+1))
+while [ $i -ne $power ]; do
+    ans=$((ans * num))
+    i=$((i + 1))
 done
 
 echo "Answer is" $ans
--- Lab4/sum_even.sh
+++ Lab4/sum_even.sh
@@ -2,10 +2,9 @@ echo "Enter a Number: "
 read num
 
 i=2
-while [ $i -lt $num ]
-do
-    sum=$((sum+i))
-    i=$((i+2)) 
+while [ $i -lt $num ]; do
+    sum=$((sum + i))
+    i=$((i + 2))
 done
 
 echo "Answer is" $sum
