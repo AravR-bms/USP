c5e8624cf29ce3a94882f0f36e896225c5728da3
Aravindh RS <aravindh.ravishankar@gmail.com>
2020-11-05 15:18:41 +0530

    Added extra case

--- Lab5/is_vowel.sh
+++ Lab5/is_vowel.sh
@@ -1,10 +1,14 @@
-echo "Enter character: "
+echo "Enter alphabet: "
 read char
 
-case $char in [aeiouAEIOU])
+case $char in
+[aeiouAEIOU])
     echo "Vowel"
     ;;
-*)
+^[aeiouAEIOU] | [a-z] | [A-Z])
     echo "Consonant"
+    ;;
+*)
+    echo "Invalid"
     ;;
 esac
